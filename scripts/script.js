24b098c5e2ff6c63b760a8bd192fed4d5e551cd6
RomanZujev <roman.zujev@gmail.com>
2022-04-18 19:02:07 +0300

    added new general menu button function

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,8 @@ import themeChange from "./themeChange.js";
 import soups from "./dishes/soups.js";
 import fried from "./dishes/fried.js";
 import baked from "./dishes/baked.js";
-import { createElement, makeMenuSection } from "./functions.js";
+import other from "./dishes/other.js";
+import { makeMenuSection, makeMISCSection } from "./functions.js";
 //Assing a theme handler to theme button
 document.querySelector(".theme_btn").onclick = themeChange;
 
@@ -13,3 +14,8 @@ var recipeStatus = false;
 makeMenuSection("dropdownS", "menuB1", "SOUPS", soups, recipeStatus);
 makeMenuSection("dropdownB", "menuB2", "BAKED", baked, recipeStatus);
 makeMenuSection("dropdownF", "menuB3", "FRIED", fried, recipeStatus);
+makeMenuSection("dropdownO", "menuB4", "OTHER", other, recipeStatus);
+
+let allDishes = [soups, fried, baked, other];
+
+makeMISCSection(allDishes, recipeStatus);
