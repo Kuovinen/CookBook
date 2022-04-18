890cee8d9d9b4caae81a797966e2a3e084506c3c
RomanZujev <roman.zujev@gmail.com>
2022-04-18 19:01:14 +0300

    added new general menu button function, fixed few minor things in other that don't affect anyting

--- scripts/functions.js
+++ scripts/functions.js
@@ -21,7 +21,7 @@ function setTitle(dishName, list) {
 }
 //create the dish illustration for recipe div
 function setIllustration(dishName, list) {
-  let illustration = document.createElement("img", "dishIllustration");
+  let illustration = document.createElement("img", "");
   illustration.src = list[dishName][3];
   illustration.alt = "RECIPE_ILLUSTRATION";
   return illustration;
@@ -29,27 +29,30 @@ function setIllustration(dishName, list) {
 //Cooking process text for recipe div
 function setMethod(dishName, list) {
   //Set up process text
-  let cookingMethod = createElement("p", "block");
+  let cookingMethod = createElement("p", "method");
   cookingMethod.textContent = list[dishName][2];
   return cookingMethod;
 }
 //Ingridients table for recipe div
 function createIngTable(dishName, list) {
-  let ingredientsTxt = createDiv("tableContainerDiv", "inline-block");
   let ingredients = createElement("table", "table");
 
   for (let i = 0; i < list[dishName][1].length; i++) {
-    let row = createElement("tr", "row" + i);
+    let row = createElement("tr", "row");
     ingredients.appendChild(row);
-    for (let j = 0; j < list[dishName][1][i].length; j++) {
-      let lineTxt = createElement("td", "td"); //cell to hold each new lines text
+    for (let j = 0; j < list[dishName][1][i].length - 1; j++) {
+      //give amount cells it's own class for CSS
+      let lineTxt;
+      j == 0
+        ? (lineTxt = createElement("td", "td tdA"))
+        : (lineTxt = createElement("td", "td"));
+
       lineTxt.textContent = list[dishName][1][i][j]; //cell content
       ingredients.childNodes[i].appendChild(lineTxt);
     }
   }
-  ingredientsTxt.appendChild(ingredients);
 
-  return ingredientsTxt;
+  return ingredients;
 }
 //MAIN FUNCTION--------------------------------------------------------------------
 //populates the"recipe" div. Content from list object
@@ -63,14 +66,12 @@ export default function createRecipe(recipe, dishName, list) {
   //fill the array wtih DOM elements
   elementArray.push(setTitle(dishName, list));
   elementArray.push(document.createElement("br"));
-  elementArray.push(createDiv("ingredientsDiv", "inline-block"));
-  elementArray.push(setMethod(dishName, list));
-  elementArray.push(document.createElement("br"));
-  //populate ingredientsDiv (elementArray[3]) with image and ingredients data
-  let illustration = setIllustration(dishName, list);
-  let ingredientsTxt = createIngTable(dishName, list);
-  elementArray[2].appendChild(illustration);
-  elementArray[2].appendChild(ingredientsTxt);
+  let container = createElement("div", "rContainer");
+  container.appendChild(setIllustration(dishName, list));
+  container.appendChild(createIngTable(dishName, list));
+  container.appendChild(setMethod(dishName, list));
+  elementArray.push(container);
+
   //Append all created elements into the "RECIPE" div
   elementArray.map((element) => {
     document.getElementsByClassName("recipe")[0].appendChild(element);
@@ -106,5 +107,29 @@ export function makeMenuSection(
       recipeStatus = createRecipe(recipeStatus, key, dishes);
     };
     node.childNodes[1].appendChild(a);
+  });
+}
+//MISC MENU FUNCTION--------------------------------------------------------------------
+
+export function makeMISCSection(dishes, recipeStatus) {
+  dishes.map((element) => {
+    //select the button container
+    let node = document.querySelector(".dropdownM");
+    node.appendChild(createElement("button", "menuB4"));
+    node.appendChild(createElement("div", "dropdown-content"));
+    node.childNodes[0].innerHTML = "MENU";
+    //fill it with <a> elements that each have a class and text
+    let keys = Object.keys(element);
+    keys.map((key) => {
+      let ilu = document.createElement("img", `tDish`);
+      ilu.className = `tDish`;
+      ilu.src = element[key][3];
+      ilu.alt = "soybeancurdimage";
+
+      ilu.onclick = function () {
+        recipeStatus = createRecipe(recipeStatus, key, element);
+      };
+      node.childNodes[1].appendChild(ilu);
+    });
   });
 }
