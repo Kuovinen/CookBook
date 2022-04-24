2fae0c02119c2fbb3c6118e2e57ed89882b8959f
RomanZujev <roman.zujev@gmail.com>
2022-04-24 17:04:54 +0300

    visual and stylistic changes, colors, borders, icons

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,15 +3,17 @@ import soups from "./dishes/soups.js";
 import fried from "./dishes/fried.js";
 import baked from "./dishes/baked.js";
 import other from "./dishes/other.js";
-import { makeMenuSection, makeMISCSection } from "./functions.js";
+import { makeMenuSection, makeMISCSection, adjustMenu } from "./functions.js";
 //Assing a theme handler to theme button
 document.querySelector(".theme_btn").onclick = themeChange;
 
-makeMenuSection("dropdownS", "menuB1", "SOUPS", soups);
-makeMenuSection("dropdownB", "menuB2", "BAKED", baked);
-makeMenuSection("dropdownF", "menuB3", "FRIED", fried);
-makeMenuSection("dropdownO", "menuB4", "OTHER", other);
+makeMenuSection("dropdownS", "menuB1", "Soups", soups);
+makeMenuSection("dropdownB", "menuB2", "Baked", baked);
+makeMenuSection("dropdownF", "menuB3", "Fried", fried);
+makeMenuSection("dropdownO", "menuB4", "Other", other);
 
 let allDishes = [soups, fried, baked, other];
 
 makeMISCSection(allDishes);
+
+adjustMenu();
--- public/scripts/themeChange.js
+++ public/scripts/themeChange.js
@@ -1,6 +1,9 @@
 var dark = false;
 export default function () {
   if (!dark) {
+    document.querySelector(".themeIcon").src = "images/sun.svg";
+    document.querySelector(".themeIcon").style.filter =
+      "invert(100%) sepia(100%) saturate(0%) hue-rotate(288deg) brightness(102%) contrast(102%)";
     document.documentElement.style.setProperty(
       "--light-color",
       "rgb(160,129,108)"
@@ -19,6 +22,9 @@ export default function () {
     );
     dark = true;
   } else {
+    document.querySelector(".themeIcon").src = "images/moon.svg";
+    document.querySelector(".themeIcon").style.filter =
+      "invert(0%) sepia(85%) saturate(7439%) hue-rotate(294deg) brightness(85%) contrast(113%)";
     document.documentElement.style.setProperty(
       "--light-color",
       "rgb(232, 222, 209)"
